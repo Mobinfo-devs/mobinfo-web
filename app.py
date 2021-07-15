205d9decb7f0ca6307b28fcb38691b24bedac070
Sohaib-50 <sohaibahmedabbasi0@gmail.com>
2021-07-15 19:20:08 +0500

    added signin page and refactored some code

--- app.py
+++ app.py
@@ -4,13 +4,24 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("layout.html")
+    return render_template("layout.html", title="Mobinfo")
 
-@app.route("/signup")
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
-    if request.args.get("name"):
-        print(request.args.get("name"))
-    return render_template("signup.html")
+    if request.method == "GET":
+        return render_template("signup.html", title="Sign Up")
+    else:  # sign up form submitted
+        pass
+        # TODO: Write signup logic
+
+@app.route("/signin", methods=["GET", "POST"])
+def signin():
+    if request.method == "GET":
+        return render_template("signin.html", title="Sign In")
+    else:  # sign in form submitted
+        pass
+        # TODO: Write signup logic
+
 
 
 
