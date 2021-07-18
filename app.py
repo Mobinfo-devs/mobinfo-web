84682195ae6e45b35f9b9820bf9e5ea21efe6a61
Sohaib-50 <sohaibahmedabbasi0@gmail.com>
2021-07-18 13:12:27 +0500

    made brands page dynamic, added rough specific brand page/functionality

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 from flask import Flask, render_template, request
+from os import getenv
+import mysql.connector
+db_connection = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = getenv("MySQLpassword"),
+    database = "mobinfo"
+)
+
+db = db_connection.cursor()
 
 app = Flask(__name__)
 
@@ -28,7 +38,35 @@ def signin():
 
 @app.route("/brands")
 def brands():
-    return render_template("brands.html")
+    db.execute(f"""
+    SELECT name, logo_url FROM brand
+    ORDER BY name;
+    """)
+    db_result = db.fetchall()
 
+    brands = []
+    for row in db_result:
+        brands.append({"name": row[0], "logo_url": row[1]})
 
+    return render_template("brands.html", brands=brands)
+
+@app.route("/brands/<brand_name>")
+def specific_brand(brand_name):
+    db.execute(f"""
+    SELECT name, logo_url, description FROM brand
+    WHERE name = %s;
+    """, (brand_name, ))
+    db_result = db.fetchone()
+    print(db_result)
+    
+    if not db_result:
+        return "Brand not found."
+
+    return render_template("brand_detail.html", brand_name=db_result[0], brand_logo_url=db_result[1], brand_description=db_result[2])
+
+# @app.errorhandler(404)
+# def not_found_error(error):
+#     return render_template('404.html', pic=pic), 404
+
+    
 app.run(debug=True)
